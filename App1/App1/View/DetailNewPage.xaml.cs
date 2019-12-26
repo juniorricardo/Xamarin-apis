5e4c6d267ff78d122e0d0fbc0862546af1efe00d
Junior Ricardo <neo.junior.16@gmail.com>
2019-12-26 13:22:18 -0300

    Caso de uso, Abrir Browser de la pagina, finalizado

--- App1/App1/View/DetailNewPage.xaml.cs
+++ App1/App1/View/DetailNewPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,11 +14,23 @@ namespace AppDemo.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailNewPage : ContentPage
     {
+        string uriNews;
         public DetailNewPage(Articles article)
         {
             InitializeComponent();
 
+            uriNews = article.url;
             BindingContext = article;
         }
+       
+        public async void openBrowserNews(object sender, EventArgs e)
+        {
+           await Browser.OpenAsync(uriNews);
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
+    
 }
